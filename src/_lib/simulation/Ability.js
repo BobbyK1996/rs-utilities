196c3355ce1a6efc104691c6615ba20d57c3f945
BobbyK1996 <bobbykamal@hotmail.co.uk>
2025-08-12 23:32:49 +0100

    updated Ability.js to be able to take in more parameters, simulating DoT effects

--- src/_lib/simulation/Ability.js
+++ src/_lib/simulation/Ability.js
@@ -1,12 +1,31 @@
 class Ability {
-  constructor(name, damage, castTimeTicks, cooldownTicks, adrenalineChange) {
+  constructor(
+    name,
+    instantDamage = 0,
+    castTimeTicks = 0,
+    cooldownTicks = 0,
+    adrenalineChange = 0,
+    dotDamage = 0,
+    dotDurationTicks = 0,
+    bypassesSunshine = false,
+    type = 'basic',
+  ) {
     this.name = name;
-    this.damage = damage;
+    this.instantDamage = instantDamage;
     this.castTimeTicks = castTimeTicks;
     this.cooldownTicks = cooldownTicks;
     this.adrenalineChange = adrenalineChange;
 
+    this.dotDamage = dotDamage;
+    this.dotDurationTicks = dotDurationTicks;
+
+    this.bypassesSunshine = bypassesSunshine;
+
+    this.type = type;
+
     this.remainingCooldown = 0;
+
+    this.activeDotTicksLeft = 0;
   }
 
   isReady() {
@@ -18,9 +37,21 @@ class Ability {
   }
 
   tickCooldown() {
-    if (this.remainingCooldown > 0) {
-      this.remainingCooldown--;
+    if (this.remainingCooldown > 0) this.remainingCooldown--;
+  }
+
+  startDot() {
+    if (this.dotDamage > 0 && this.dotDurationTicks > 0)
+      this.activeDotTicksLeft = this.dotDurationTicks;
+  }
+
+  tickDot() {
+    if (this.activeDotTicksLeft > 0) {
+      this.activeDotTicksLeft--;
+
+      return this.dotDamage / this.dotDurationTicks;
     }
+    return 0;
   }
 }
 
